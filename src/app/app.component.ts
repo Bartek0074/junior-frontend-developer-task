d41476356df04c81d828fba75ec3393e21f4f454
Bartek0074 <bartekdo123@gmail.com>
2025-04-29 22:37:11 +0200

    fix: bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,37 @@ import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NgClass,TaskItemComponent],
+  imports: [NgClass, TaskItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  standalone: true
+  standalone: true,
 })
 export class AppComponent {
   title = 'junior-frontend-developer-task';
 
   protected tasks = [
-    { name: 'Zrobić zakupy spożywcze', status: 'Completed', date: '2025-05-01', description: 'Muszę kupić mleko, mąkę i jajka.' },
-    { name: 'Opłacić rachunki', status: 'Pending', date: '2025-05-10', description: 'Tylko nie odkładaj tego na inny dzień!' },
-    { name: 'Urodziny mamy', status: 'Planned', date: '2025-05-15', description: 'Kupić kwiaty i tort.' }
+    {
+      name: 'Zrobić zakupy spożywcze',
+      status: 'Completed',
+      date: '2025-05-01',
+      description: 'Muszę kupić mleko, mąkę i jajka.',
+    },
+    {
+      name: 'Opłacić rachunki',
+      status: 'Pending',
+      date: '2025-05-10',
+      description: 'Tylko nie odkładaj tego na inny dzień!',
+    },
+    {
+      name: 'Urodziny mamy',
+      status: 'Planned',
+      date: '2025-05-15',
+      description: 'Kupić kwiaty i tort.',
+    },
   ];
+  protected descVisible: boolean = false;
+
+  toggleCompleted(task: any) {
+    task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
+  }
 }
