669966d3ef98d5e31cedb1c519955cc1d0fc0c0a
Bartek0074 <bartekdo123@gmail.com>
2025-04-29 22:21:52 +0200

    first commit

---
+++ src/app/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgIf, NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [NgIf, NgClass],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.scss',
+})
+export class TaskItemComponent {
+  @Input() task!: { name: string; status: string; date: string; description: string };
+  @Output() toggle = new EventEmitter<void>();
+
+  descVisible: boolean = false;
+
+  toggleCompleted() {
+    this.toggle.emit();
+  }
+
+  toggleDescription() {
+    this.descVisible = !this.descVisible;
+  }
+}
